059367708d4c37acae989cf24324e2acab99440e
jRydell <johannilsrydell89@gmail.com>
2025-02-28 14:56:00 +0100

    added vitest coverage and ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
+import tailwindcss from "@tailwindcss/vite";
+import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./app/_tests_/setup.ts",
+    exclude: [...configDefaults.exclude, "node_modules/"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
+  },
 });
