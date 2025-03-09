9e20a24c1e18b6326e99707065016b885cbb8c44
jRydell <johannilsrydell89@gmail.com>
2025-03-09 19:13:20 +0100

    small fix

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="max-w-2xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4">Project Unknown</h1>
+      <h1 className="text-3xl font-bold mb-4">Work in progress</h1>
     </div>
   );
 };
