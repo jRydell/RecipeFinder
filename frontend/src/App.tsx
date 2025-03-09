5027c5fb041d063baf0639ec480bbe9f36f8dd31
jRydell <johannilsrydell89@gmail.com>
2025-03-09 19:19:54 +0100

    import fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Health from "./pages/HealthCheck";
-import "./Index.css";
+import "./App.css";
 import NotFound from "./pages/NotFound";
-
-// Define a NotFound component
 
 const router = createBrowserRouter([
   {
