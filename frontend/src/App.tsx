d09efb821d22cce33cc8d9d59d41bfd4a5c1f874
jRydell <johannilsrydell89@gmail.com>
2025-03-09 19:10:11 +0100

    add Home and NotFound pages, implement routing with Layout component,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,32 @@
-import Health from "./components/HealthCheck/HealthCheck";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import Health from "./pages/HealthCheck";
+import "./app.css";
+import NotFound from "./pages/NotFound";
+
+// Define a NotFound component
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "health",
+        element: <Health />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Project Unknown</h1>
-      </header>
-      <main>
-        <Health />
-      </main>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+const NotFound = () => (
+  <div className="text-center py-10">
+    <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
+    <p>The page you're looking for doesn't exist.</p>
+  </div>
+);
+
+export default NotFound;
